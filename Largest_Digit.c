9ea0ff3f5578129ca322e960c43bee37333a1e93
tejaswinikanda2003 <97425786+tejaswinikanda2003@users.noreply.github.com>
2022-04-22 16:35:29 +0530

    #include<stdio.h> int main()//largest {     int n,i=0,rem,k;     scanf("%d",&n);     while(n!=0)     {         rem=n%10;         n=n/10;         k=rem;         if(k>i)         {             i=k;         }     }     printf("%d",i); }

--- Largest_Digit.c
+++ Largest_Digit.c
@@ -1,34 +1,21 @@
 #include<stdio.h>
-
-int main()
-
+int main()//largest
 {
-
     int n,i=0,rem,k;
-
     scanf("%d",&n);
-
     while(n!=0)
-
     {
-
         rem=n%10;
-
         n=n/10;
-
         k=rem;
-
         if(k>i)
-
         {
-
             i=k;
-
         }
-
     }
-
     printf("%d",i);
-
-
 }
+
+
+
+
