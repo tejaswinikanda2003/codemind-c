3d7295a129cf431ecf1eb3ceefb6002af00795bc
Tejaswini <97425786+tejaswinikanda2003@users.noreply.github.com>
2022-05-02 18:19:51 +0530

    #include <stdio.h> int main() {
        char s[1000];
        scanf("%[^
    ]s", &s);
      	printf("Hello Technicalhub
    ");
          printf("%s",s);
        /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
        return 0;
    }

---
+++ Hello_Technicalhub.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+{
+	
+    char s[1000];
+    scanf("%[^
+]s", &s);
+  	printf("Hello Technicalhub
+");
+      printf("%s",s);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
