237a379cc3f5c326e696dfdfec3bff65fc29e6d0
Tejaswini <97425786+tejaswinikanda2003@users.noreply.github.com>
2022-06-28 14:57:41 +0530

    #include<stdio.h> int main() { 	int i,n,sum=0; 	scanf("%d",&n); 	for(i=1;i<n;i++) 	{ 	 if (n%i==0) 	 { 	 sum+=i; 	 } 	} 	if(sum==n) 	{ 	    printf("True"); 	} 	else 	{ 	    printf("False"); 	} }

--- Perfect_Number.c
+++ Perfect_Number.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-int i,n,sum=0;
-	scanf("%d",&n);//med perfect num
+	int i,n,sum=0;
+	scanf("%d",&n);
 	for(i=1;i<n;i++)
 	{
 	 if (n%i==0)
@@ -19,5 +19,3 @@ int i,n,sum=0;
 	    printf("False");
 	}
 }
-
-
